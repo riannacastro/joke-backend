6c3fc7000ba26f837f4542efbce853d36fa41c21
Rianna Castro <riannaxcastro@gmail.com>
2021-12-09 13:33:37 -1000

    added show method to jokes

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -4,4 +4,13 @@ class JokesController < ApplicationController
         render json: jokes
     end
 
+    def show
+        joke = Joke.find_by_id(params[:id])
+        render json: joke
+    end
+
+    def create
+    end
+
+
 end
