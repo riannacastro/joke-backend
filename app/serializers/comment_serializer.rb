824b29e88f28084f6e2881723f612e8c62e83df5
Rianna Castro <riannaxcastro@gmail.com>
2021-12-10 18:32:21 -1000

    changes to serializers

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,4 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :name, :comment
-  belongs_to :user
   belongs_to :joke
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :username
   has_many :jokes
-  has_many :comments, through: :jokes
+  
 end
