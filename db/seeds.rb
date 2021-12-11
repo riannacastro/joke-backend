cfd13a0ca109a87c182874f2bc5354bfe53daa78
Rianna Castro <riannaxcastro@gmail.com>
2021-12-11 10:49:48 -1000

    fixed backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,11 @@
 # steve = User.create(name: "Steve", username:"iamsteve_97");
 # ri = User.create(name: "ri", username:"riri");
 
-joke1 = Joke.create(joke: "Why did the programmer quit his job? Because he didn't get arrays.");
-joke2 = Joke.create(joke: "Real programmers count from 0.");
-joke3 = Joke.create(joke: "Two SQL tables sit at the bar. A query approaches and asks, \"Can I join you?\"");
-joke4 = Joke.create(joke: "\"Honey, go to the store and buy some eggs.\"\n\"OK.\"\n\"Oh and while you're there, get some milk.\"\nHe never returned.");
-joke5 = Joke.create(joke: "Knock knock.\nWho's there?\nRecursion.\nRecursion who?\nKnock knock.");
+Joke.create(joke: "Why did the programmer quit his job? Because he didn't get arrays.");
+Joke.create(joke: "Real programmers count from 0.");
+Joke.create(joke: "Two SQL tables sit at the bar. A query approaches and asks, \"Can I join you?\"");
+Joke.create(joke: "\"Honey, go to the store and buy some eggs.\"\n\"OK.\"\n\"Oh and while you're there, get some milk.\"\nHe never returned.");
+Joke.create(joke: "Knock knock.\nWho's there?\nRecursion.\nRecursion who?\nKnock knock.");
 
 # Comment.create(name: "Kathy", comment:"LOL", joke: joke1);
 # Comment.create(name: "David", comment:"wait this one is funny hahahah", joke: joke3);
