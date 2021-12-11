881400e3cbc92fbbde1a98d7a568630196ebccef
Rianna Castro <riannaxcastro@gmail.com>
2021-12-11 10:43:34 -1000

    reseed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_12_09_222723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jokes", force: :cascade do |t|
+    t.string "joke"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
