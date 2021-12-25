cbf4b97821b909bd8d93c651ce77a340487882f4
Rianna Castro <riannaxcastro@gmail.com>
2021-12-24 20:03:17 -1000

    add methods in requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,4 +2,22 @@ class RequestsController < ApplicationController
     def index
         requests = Request.all
         render json: requests
+    end
+
+    def create
+        request = Request.new(request_params)
+        
+        if request.save
+            render json: request
+        else
+            render json: {error: "Request could not be saved."}
+        end
+    end
+
+    private 
+    
+    def request_params
+        params.require(:request).permit(:username, :request)
+    end
+
 end
